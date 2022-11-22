73a931da7668a215dac62206d55dbc331a4e654b
charrrleee <charlcpy@gmail.com>
2022-11-22 10:36:26 -0500

    implement todo firebase function

--- MAPD714-TodoList/Database/TodoDatabase.swift
+++ MAPD714-TodoList/Database/TodoDatabase.swift
@@ -9,19 +9,78 @@ import FirebaseFirestore
 import UIKit
 import Foundation
 import FirebaseCore
+//import RxSwift
 
-
-class TodoDatabase
+class TodoDatabase : ObservableObject
 {
-    init() {
+    var dbName = "todos";
+//    var todos : Observable<[Todo]>
+    var todoList: [Todo] = []
+
+    init()
+    {
+        self.getTodoList(true)
+//        self.insertTodo(
+//            Todo(
+//                name: "abc",
+//                isCompleted: true,
+//                notes: "hahhaha",
+//                hasDueDate: true ,
+//                dueDate: Calendar.current.date(byAdding: .day, value: 5, to: Date())
+//            )
+//        )
+//        self.insertTodo(
+//            Todo(
+//                name: "cba",
+//                isCompleted: false,
+//                notes: "lalal",
+//                hasDueDate: false
+//            )
+//        )
+    }
+    
+    func getTodoList(_ isCompleted: Bool)
+    {
 
         let db = Firestore.firestore()
+        db.collection(self.dbName).getDocuments()
+        { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                    let item = Todo(
+                        name: document.data()["name"] as! String,
+                        isCompleted: document.data()["isCompleted"] as! Bool,
+                        notes: document.data()["notes"] as! String,
+                        hasDueDate: document.data()["hasDueDate"] as! Bool,
+                        dueDate: document.data()["dueDate"] as? Date
+                    )
+                    // todo todo list should be observable
+                    self.todoList.append(item)
+                    print("\(document.documentID) => \(document.data()) => \(document)")
+                }
+            }
+        }
+    }
+    
+    func insertTodo(_ todo: Todo)
+    {
+        let db = Firestore.firestore()
         var ref: DocumentReference? = nil
-        ref = db.collection("users").addDocument(data: [
-            "first": "Ada",
-            "last": "Lovelace",
-            "born": 1815
-        ]) { err in
+        var data: [String: Any]  = [
+            "name": todo.name,
+            "isCompleted": todo.isCompleted,
+            "notes": todo.notes,
+            "hasDueDate": todo.hasDueDate
+        ]
+        if (todo.hasDueDate)
+        {
+            data["dueDate"] = todo.dueDate! as Date
+        }
+
+        ref = db.collection(self.dbName)
+            .addDocument(data: data) { err in
             if let err = err {
                 print("Error adding document: \(err)")
             } else {
